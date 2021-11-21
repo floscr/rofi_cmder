9190c36ef1a2bf279b0b977d5023ee5e2c9aae5b
Florian Schroedl <flo.schroedl@gmail.com>
2021-11-21 18:19:12 +0100

    Refactor: Convert to EitherE

--- src/lib/commands.nim
+++ src/lib/commands.nim
@@ -20,11 +20,11 @@ proc getCommandsConfigDir(): string =
   .joinPath(constants.CONFIG_DIRNAME)
   .joinPath(constants.COMMANDS_CONFIG_FILENAME)
 
-proc getCommands*(path: string = getCommandsConfigDir()): EitherS[seq[ConfigItem]] =
-  trySt(readFile(path))
-  .flatMap((x: string) => trySt(
+proc getCommands*(path: string = getCommandsConfigDir()): EitherE[seq[ConfigItem]] =
+  tryET(readFile(path))
+  .flatMap((x: string) => tryET(
     parseJson(x).getElems()
   ))
-  .flatMap((xs: seq[JsonNode]) => trySt(
+  .flatMap((xs: seq[JsonNode]) => tryET(
     xs.map((x: JsonNode) => to(x, ConfigItem)))
   )
