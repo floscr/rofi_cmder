d8a55d345e20147a1ac825e9d04c05219fb71831
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Add helper methods

--- src/lib/utils/fp.nim
+++ src/lib/utils/fp.nim
@@ -1,5 +1,6 @@
 import std/osproc
 import std/strutils
+import std/sugar
 import fp/either
 import print
 
@@ -18,3 +19,19 @@ proc sh*(cmd: string, opts = {poStdErrToStdOut}): Either[string, string] =
   return res
     .strip
     .left(string)
+
+proc asSeq*[E,A](e: Either[E,A]): seq[A] =
+  ## Converts Either to List
+  if e.isLeft:
+    @[]
+  else:
+    @[e.get()]
+
+proc filter*[E,A](v: Either[E, A], cond: A -> bool): Either[A,A] =
+  ## Convert to Left unless the `cond` is true
+  if v.isRight() and cond(v.get()):
+    v
+  elif v.isRight():
+    v.get().left(E)
+  else:
+    v
