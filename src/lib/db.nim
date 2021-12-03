f01f7212d6e0e5fcc8f104954251426bd69fbdfa
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Sketch out db update/insert

--- src/lib/db.nim
+++ src/lib/db.nim
@@ -1,25 +1,42 @@
 import std/tables
+import std/asyncdispatch
 import std/os
+import std/tempfiles
 import std/sugar
 import std/strutils
 import std/strformat
 import std/sequtils
+import std/strutils
+import std/streams
+import std/strformat
+import std/os
+import std/times
 import fp/tryM
 import fp/either
 import fusion/matching
 import constants
 import print
 import zero_functional
+import cascade
 
 {. experimental: "caseStmtMacros" .}
 
-type count* = int
+const DB_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:sszzz"
 
+proc dbDate*(x: DateTime): string =
+  x.format(DB_TIME_FORMAT)
+
+proc dbDate*(): string {.inline.} =
+  utc(now()).dbDate()
+
+type countT* = int
+type timeT* = string
+type dataT* = string
 type
-  DbItem* = ref object
-    count*: count
-    time*: string
-    data*: string
+  DbItem* = object
+    count*: countT
+    time*: timeT
+    data*: dataT
 
 proc `$`*(x: DbItem): string =
      &"""DbItem(
@@ -28,12 +45,22 @@ proc `$`*(x: DbItem): string =
     data: {x.data},
 )"""
 
-proc getDbPath(): string =
-  getCacheDir()
-  .joinPath(constants.CACHE_DIRNAME)
-  .joinPath(constants.CACHE_FREECE_DB_FILENAME)
+proc increment(x: DbItem): DbItem =
+  cascade x:
+    count = x.count + 1
 
-proc parseLine(x: string): DbItem =
+proc createDbItem(data: dataT): DbItem =
+  DbItem(
+    count: 0,
+    time: dbDate(),
+    data: data,
+  )
+
+proc toCsvRowString(x: DbItem): string =
+  (count: @count, time: @time, data: @data) := x
+  &"{count},{time},{data}"
+
+proc fromCsvRowString(x: string): DbItem =
   [@count, @time, @data] := x.split(",", maxsplit = 2)
   DbItem(
     count: count.parseInt,
@@ -41,8 +68,16 @@ proc parseLine(x: string): DbItem =
     data: data,
   )
 
-proc parseLinesAsMap(xs: seq[string]): OrderedTable[count, seq[DbItem]] =
-  xs --> map(parseLine)
+proc getDbPath(): string =
+  getCacheDir()
+  .joinPath(constants.CACHE_DIRNAME)
+  .joinPath(constants.CACHE_FREECE_DB_FILENAME)
+
+# proc readDb(path: string = getDbPath()): auto =
+#   fromEither(tryET do: lines path)
+
+proc parseLinesAsMap(xs: seq[string]): OrderedTable[countT, seq[DbItem]] =
+  xs --> map(fromCsvRowString)
   .group(it.count)
 
 proc prepareText(x: string): seq[string] =
@@ -50,12 +85,46 @@ proc prepareText(x: string): seq[string] =
   y.stripLineEnd()
   y.splitLines()
 
-proc readDb*(dbPath: string = getDbPath()): auto =
-  tryET(readFile(dbPath))
-  .map(prepareText)
-  .flatMap((xs: seq[string]) => tryET(
-    parseLinesAsMap(xs)
-  ))
+# proc updateDb*(dbPath: string = getDbPath()): auto =
+
+# proc readDbLines(dbLines = readDb()): auto =
+#   dbLines
 
 when isMainModule:
-  echo readDb("/home/floscr/.cache/cmder_history.db")
+  proc updateFile(data: string): auto =
+    let dbPath = "/tmp/foo"
+    let db = openFileStream(dbPath, fmRead)
+
+    var output = ""
+    var line = ""
+    var hasUpdateRow = false
+
+    while db.readLine(line):
+      let item = fromCsvRowString(line)
+
+      if item.data == data:
+        output &= item.increment().toCsvRowString() & "\n"
+        hasUpdateRow = true
+        continue
+
+      output &= line & "\n"
+
+    if not hasUpdateRow:
+      output &= createDbItem(data).toCsvRowString()
+
+    db.close()
+
+    let (cfile, path) = createTempFile("tmpprefix_", "_end.tmp")
+    cfile.write(output)
+    cfile.setFilePos(0)
+    close cfile
+
+    removeFile(dbPath)
+    moveFile(path, dbPath)
+
+  updateFile("<span>d​ddrun​</span>")
+
+  # "/home/floscr/.cache/cmder_history.db".lines --> filter(not it.isEmptyOrWhitespace)
+  # .map(fromCsvRowString)
+  # .createIter(errorLines)
+  # errorLines() --> foreach(echo it)
