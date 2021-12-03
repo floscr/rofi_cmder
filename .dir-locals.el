6e333e77ead06704745db23ff3c4343ccda1e6c1
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Add projectile config via dir-locals to enable alternate file switching

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((projectile-project-project-file . "rofi_cmder.nimble")
+         (projectile-project-name . "Rofi Cmder")
+         (projectile-project-type . nim)
+         (projectile-test-prefix . "test_"))))
