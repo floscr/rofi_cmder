7935bb10b9025b25af007733b645ed2a7110eee7
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+_direnv_hook_enabled=1
+_direnv_hook() {
+    if [ $_direnv_hook_enabled == "1" ]; then
+        eval "$(direnv export bash)"
+    fi
+};
+
+direnv-stop() {
+    pushd $(pwd) > /dev/null
+    cd
+    _direnv_hook_enabled=0
+    eval "$(direnv export bash)"
+    popd > /dev/null
+}
+direnv-start() {
+    echo "direnv: enabling shell hook"
+    _direnv_hook_enabled=1
+}
+
+direnv-stop
+rm -rf errors.log
+nix run .#rofi_cmder_2 --print-build-logs
+direnv-start
