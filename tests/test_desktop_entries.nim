5ee076cbca78f520e7e15947a5f8731a66a690da
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Add unit test

---
+++ tests/test_desktop_entries.nim
@@ -0,0 +1,20 @@
+import std/os
+import std/sequtils
+import std/unittest
+
+import ../src/lib/desktop_entries
+
+suite "desktop_entries":
+  test "File parsing":
+    let entries: seq[DesktopEntry] = getDesktopApplications(@[getCurrentDir().joinPath("./data")])
+    check: entries.len == 2
+
+    # First entry
+    check: entries[0].entryName == "Desktop Entry"
+    check: entries[0].name == "The Desktop Entry Name"
+    check: entries[0].exec == "command %U"
+
+    # First entry
+    check: entries[1].entryName == "Desktop Action new-window"
+    check: entries[1].name == "New Window"
+    check: entries[1].exec == """echo Symbol between "%u" params"""
