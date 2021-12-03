5700210c161fe2c6b6551b0b85f9f2875373fe76
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Cleanup: Remove unused method

--- src/lib/utils/fp.nim
+++ src/lib/utils/fp.nim
@@ -6,10 +6,6 @@ import print
 
 type Result* = enum Ok, Error
 
-proc log*(x: auto): auto =
-  print(x)
-  x
-
 proc sh*(cmd: string, opts = {poStdErrToStdOut}): Either[string, string] =
   let (res, exitCode) = execCmdEx(cmd, opts)
   if exitCode == 0:
